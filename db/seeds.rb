1afd63c9e193d2bcba898a6e49d24abae5b9255d
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-03-17 19:09:32 +0100

    Ajout Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do |city|
+  City.create(city_name: Faker::Games::Pokemon.location)
+end
+
+5.times do |tag|
+  Tag.create(tag_name: Faker::Restaurant.type)
+end
+
+20.times do |user|
+  User.create(email: (Faker::Superhero.power + "@blabla.com"), first_name: Faker::Superhero.prefix, last_name: Faker::Superhero.suffix, birth_day: Faker::Date.birthday(min_age:18, max_age:100), gender: "unknown", city_id: rand(1..5))
+end
+
+30.times do |event|
+  Event.create(title: Faker::Food.dish, description: Faker::Food.description, date: Faker::Date.forward(days: 100), guests_number: rand(1..6), city_id: rand(1..5))
+end
+
+60.times do |filter|
+  Filter.create(event_id: rand(1..30), tag_id: rand(1..5))
+end
+
+30.times do |appointment|
+  Appointment.create(user_id: rand(1..20), event_id: rand(1..30))
+end
