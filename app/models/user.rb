7341d6234099972a5d4785d62852a1328093a560
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-03-18 11:45:15 +0100

    Ajout class_name host

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   belongs_to :city
   has_many :appointments
   has_many :events, through: :appointments
+  has_many :created_events, foreign_key: "host_id", class_name: "Event"
+
 end
