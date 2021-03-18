11dcd0c7f64e4286e433c742caecbfc05edd140d
Jeanne Colmou <jeannecolmou@macbook-pro-de-jeanne.home>
2021-03-18 12:37:50 +0100

    mailer automatique user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,10 @@ class User < ApplicationRecord
   belongs_to :city
   has_many :appointments
   has_many :events, through: :appointments
+  after_create :welcome_send
+
+  def welcome_send
+  	UserMailer.welcome(self).deliver_now
+  end
+
 end
