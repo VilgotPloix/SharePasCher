c020cefdd4482b00e8c4b5950305ab0ecdcfff49
chendanet <chenda_net@yahoo.fr>
2021-03-19 18:40:38 +0100

    maj navbar

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,6 +3,10 @@ class EventsController < ApplicationController
     @event = Event.all
   end
 
+  def new
+    @event = Event.new
+  end
+
   def show
     @event = Event.find(params[:id])
   end
@@ -11,8 +15,17 @@ class EventsController < ApplicationController
   end
 
   def create
+    @event = Event.new(event_params)
+    @event.save
+  redirect_to events_path
   end
 
   def destroy
   end
+
+  private
+    def event_params
+      params.require(:event).permit(:title, :description, :city, :date, :guest_number)
+    end
+
 end
