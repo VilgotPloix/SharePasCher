013b90a1b2d988f2189264df0ec57db03606e930
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-03-19 22:13:17 +0100

    Ajout validation d'un voyageur

--- app/controllers/appointment_controller.rb
+++ app/controllers/appointment_controller.rb
@@ -15,8 +15,20 @@ class AppointmentController < ApplicationController
   end
 
   def update
+    @user = params[:user_id]
+    @event_id = params[:event_id]
+    @event = Event.find(@event_id)
+    @appointment = Appointment.where(user_id: @user, event_id: @event)
+    @appointment.update(is_accepted: true)
+    Event.find(@event_id).update(current_guests: (@event.current_guests + 1))
+    redirect_to event_path(@event_id)
   end
 
   def destroy
+    @user = params[:user_id]
+    @event = params[:event_id]
+    @appointment = Appointment.where(user_id: @user, event_id: @event)
+    @appointment.destroy_all
+    redirect_to event_path(@event)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 	root 'static#index'
-  resources :appointment, only: [:index, :create, :show, :new, :update, :destroy]
-
-  resources :events, only: [:index, :new, :show, :edit, :create, :destroy]
-  devise_for :users
-	
+  resources :appointment
 
   resources :events do
     collection do
