ee72dff95d0f03a425b377f52be0f440952eb3a8
gustave ploix <vilgotploix@gmail.com>
2021-03-19 13:28:39 +0100

    continuation du front des events + ajout d'un controller appointment

---
+++ app/controllers/appointment_controller.rb
@@ -0,0 +1,20 @@
+class AppointmentController < ApplicationController
+  def index
+  end
+
+  def new
+    
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :appointment, only: [:index, :create, :show, :new, :update, :destroy]
   resources :events, only: [:index, :show, :edit, :create, :destroy]
   devise_for :users
 	root 'static#index'
