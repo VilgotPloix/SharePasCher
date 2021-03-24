02f41f21a1592499de3c8cb470ab981b8ee4f1e3
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-03-24 18:44:18 +0100

    filter at event creation

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,10 @@ class EventsController < ApplicationController
 
   def create
     @event = Event.create(title: params[:title], description: params[:description], date: params[:date], city_id: params[:city_id], guests_number: params[:guests_number], host_id: params[:host_id], current_guests: 0)
-    # @event.save
+    @test= 0
+    puts @test
+    filter_creation(params, @event)
+    puts @test
     if @event.host.is_host == false
       User.find(@event.host.id).update(is_host: true)
       redirect_to created_index_events_path, warning: "Vous avez crée un évènement"
@@ -47,6 +50,14 @@ class EventsController < ApplicationController
   private
     def event_params
       params.require(:event).permit(:title, :description, :city, :date, :guest_number)
+    end
+
+    def filter_creation(params, event)
+      for n in (0..Tag.all.count)
+        if params[:"tag#{n}"] == "1"
+          Filter.create(event_id: event.id, tag_id: (n+1))
+        end
+      end
     end
 
 
