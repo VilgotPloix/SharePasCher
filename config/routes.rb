932b689bc02521dbd4557a2e2213d94539428d2f
Jeanne Colmou <jeannecolmou@macbook-pro-de-jeanne.home>
2021-03-25 23:57:49 +0100

    ajout des routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations" }
 	root 'static#index'
   resources :appointment
+	get '/Footer_apropos', to: 'static#Footer_apropos'
+	get '/Footer_reseaux', to: 'static#Footer_reseaux'
+	get '/Footer_confidentialite', to: 'static#Footer_confidentialite'
+	get '/Footer_cookies', to: 'static#Footer_cookies'
 
   resources :events do
     collection do
