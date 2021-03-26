9aa6e2f6f2053ec90a61d8c89ba5e7626f40917a
EmmanuelQuere <emmanuel.quere29@gmail.com>
2021-03-26 16:42:57 +0100

    Ajout specs

--- spec/models/city_spec.rb
+++ spec/models/city_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe City, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do 
+    @city = City.create(city_name: "Paris")
+  end
+
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@city).to be_a(City)
+      expect(@city).to be_valid
+    end
+
+  end
+
+  context "associations" do
+
+    describe "city" do
+      it "should have_many events" do
+        host = User.create(first_name: "Michel", last_name: "Host", email: "michel.host@test.com", password: "ceciestuntest", birth_day: "05/06/1992", gender: "H", is_host: true)
+        event = Event.create(title: "Petit repas", description: "Un bon petit repas entre amis", guests_number: 5, city: @city, host_id: host.id)
+        expect(@city.events.include?(event)).to eq(true)
+      end
+    end
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do 
+    @user = User.create(first_name: "Michel", last_name: "Bar", email: "michel.bar@test.com", password: "ceciestuntest", birth_day: "05/06/1992", gender: "H", is_host: false)
+    @host = User.create(first_name: "Michel", last_name: "Host", email: "michel.host@test.com", password: "ceciestuntest", birth_day: "05/06/1992", gender: "H", is_host: true)
+    @city = City.create(city_name: "Paris")
+    @event = Event.create(title: "Petit repas", description: "Un bon petit repas entre amis", guests_number: 5, city: @city, host_id: @host.id)
+  end
+
+  context "validation" do
+
+    it "is valid with valid attributes" do
+      expect(@user).to be_a(User)
+      expect(@user).to be_valid
+    end
+
+    describe "#first_name" do
+      it "should not be valid if not string" do
+        bad_user = User.create(first_name: 56)
+        expect(bad_user).not_to be_valid
+      end
+    end
+
+    describe "#last_name" do
+      it "should not be valid if not string" do
+        bad_user = User.create(last_name: 42)
+        expect(bad_user).not_to be_valid
+      end
+    end
+
+    describe "#gender" do
+      it "should not be valid if it is not a string" do
+        invalid_user = User.create(first_name: "Michel", last_name: "Bar", email: "michel.bar@test.com", password: "ceciestuntest", birth_day: "05/06/1992", gender: 53)
+        expect(invalid_user).not_to be_valid
+      end
+    end
+
+  end
+
+  context "associations" do
+
+    describe "events" do
+      it "should have_many events through appointments" do
+        appointment = Appointment.create(user: @user, event: @event)
+        expect(@user.appointments.include?(appointment)).to eq(true)
+        expect(@user.events.include?(@event)).to eq(true)
+      end
+    end
+
+    describe "host" do
+      it "should be a user that hosts an event " do
+        hosted_event = Event.create(title: "Petit repas", description: "Un bon petit repas entre amis", guests_number: 5, city: @city, host_id: @host.id)
+        expect(@host.created_events.include?(hosted_event)).to eq(true)
+      end
+    end
+
+  end
+
+
+
+  context "public instance methods" do
+
+    describe "#is_profile_fully_completed" do
+
+      it "it should return the correct boolean" do
+        user_1 = @user
+        expect(user_1.is_profile_fully_completed).to eq(true)
+        user_2 = User.create(first_name: "Michel", last_name: "Bar", email: "michel.bar@test.com", password: "ceciestuntest", gender: "H", is_host: false)
+        expect(user_2.is_profile_fully_completed).to eq(false)
+      end
+    end
+
+  end
+
 end
